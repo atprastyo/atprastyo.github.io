58149f6c979a91c688c3a067b2f93d4d75eedc1c
atprastyo <atprastyo19@gmail.com>
2021-10-15 06:14:42 +0700

    refactor: remove unused

--- lib/theme/responsive.dart
+++ lib/theme/responsive.dart
@@ -3,11 +3,6 @@ import 'package:flutter/material.dart';
 // We will modify it once we have our final design
 
 class Responsive extends StatelessWidget {
-  final Widget mobile;
-  final Widget? mobileLarge;
-  final Widget? tablet;
-  final Widget desktop;
-
   const Responsive({
     Key? key,
     required this.mobile,
@@ -15,6 +10,11 @@ class Responsive extends StatelessWidget {
     required this.desktop,
     this.mobileLarge,
   }) : super(key: key);
+
+  final Widget mobile;
+  final Widget? mobileLarge;
+  final Widget? tablet;
+  final Widget desktop;
 
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width <= 500;
