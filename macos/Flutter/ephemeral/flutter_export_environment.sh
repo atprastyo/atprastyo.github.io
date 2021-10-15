5a8df8b16cb78c4b9c0506f81bd85843b48712a4
atprastyo <atprastyo19@gmail.com>
2021-10-15 22:53:28 +0700

    style: change fav icon

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/agust/development/engine/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/agust/development/mobile/flutter/my-profile/my_profile_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
